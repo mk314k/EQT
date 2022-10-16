3a3eb9cf73d5c59e0d1998630865750c80b7ac53
Kartikesh Mishra <mishrakesh3.14kartik@gmail.com>
2022-10-16 06:28:38 -0400

    tutee oop

---
+++ src/Tutee.ts
@@ -0,0 +1,10 @@
+export class Tutee{
+    public constructor(public readonly name:string, public readonly subjects:Array<string>, public readonly availabilty:Array<Boolean>){
+
+    }
+    public generateHTML():string{
+        return `
+        
+        `
+    }
+}

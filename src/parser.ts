9f6e32285e29bdc2000d559543b441b16d056d0d
Kartikesh Mishra <mishrakesh3.14kartik@gmail.com>
2022-10-16 07:50:35 -0400

    debugging Parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,12 +1,14 @@
 import assert from "assert";
 
 export function Parser(userData: string): Array<Array<string>>{
+    console.log(userData);
     var result = new Array();
     const split1 = userData.split('\n');
     var split2: Array<string>;
-    for (const field in split1){
+    for (const field of split1){
         split2 = field.split('|');
         result.push(split2);
     }
+    console.log(result);
     return result;
 }
